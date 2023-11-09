3b3033d7d6faf3ce2ecff00dab1d8467aaeb0baf
Moikapy <warrenckhan@gmail.com>
2023-11-08 23:49:49 -0500

    feat: Update Header component with new button and link
    - Added import statement for Link from 'next/link'
    - Updated margin-bottom value in media query to 75px
    - Added Button styled component with hover effect
    - Added Shop button with Link to external URL

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import {sizes, devices} from '@/styles/index';
+import Link from 'next/link';
 
 const _Header = styled.div`
   display: flex;
@@ -11,7 +12,7 @@ const _Header = styled.div`
 
   @media ${devices.root} {
     margin-top: 150px;
-    margin-bottom: 150px;
+    margin-bottom: 75px;
   }
   @media ${devices.laptop} {
     margin-top: 250px;
@@ -50,12 +51,33 @@ const CTA = styled.p`
   }
 `;
 
+const Button = styled.button`
+  margin: 1rem 5px;
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+  border: 1px solid #000;
+  font-size: 16px;
+  font-weight: 600;
+  color: #000;
+  background: #fff;
+  text-transform: uppercase;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+  &:hover {
+    background: #000;
+    color: #fff;
+  }
+`;
+
 export default function Header() {
   return (
     <_Header>
       {' '}
       <Brand_name>MOIKAS</Brand_name>
       <CTA>Gain the latest insights and discoveries in AI.</CTA>
+      <Button>
+        <Link target='_self' href={'https://moikas.com/shop'}>Shop</Link>
+      </Button>
     </_Header>
   );
 }
