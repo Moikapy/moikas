f32870c64b741484e5384414ae5ecd9f100c9911
Moikapy <warrenckhan@gmail.com>
2023-11-12 23:24:08 -0500

    feat: Add reusable Editor component
    This commit adds a new reusable Editor component to the TextEditor file. The Editor component is registered as a Client Component and can be used in other parts of the code. It creates a new editor instance using BlockNoteEditor and renders it using the BlockNoteView React component.

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,10 +1,12 @@
+'use client'; // this registers <Editor> as a Client Component
 import {BlockNoteEditor} from '@blocknote/core';
 import {BlockNoteView, useBlockNote} from '@blocknote/react';
 import '@blocknote/core/style.css';
 
-export default function TextEditor() {
+// Our <Editor> component we can reuse later
+export default function Editor() {
   // Creates a new editor instance.
-  const editor: BlockNoteEditor = useBlockNote({});
+  const editor: BlockNoteEditor | null = useBlockNote({});
 
   // Renders the editor instance using a React component.
   return <BlockNoteView editor={editor} />;
