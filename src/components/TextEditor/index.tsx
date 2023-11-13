3bd8da3db516e36b0c19aa593ac89a22672f33dd
Moikapy <warrenckhan@gmail.com>
2023-11-12 23:34:53 -0500

    feat: Add initial content and content storage to Editor component
    This commit adds the ability to retrieve previously stored editor contents and store new contents in local storage. The initialContent variable retrieves the stored content, if any, from local storage. The useBlockNote hook is updated with an onEditorContentChange callback that saves the top-level blocks of the editor in local storage whenever there is a change. Additionally, the setBlocks function is called to update the state with the current top-level blocks.

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,12 +1,26 @@
 'use client'; // this registers <Editor> as a Client Component
-import {BlockNoteEditor} from '@blocknote/core';
+import {Block, BlockNoteEditor} from '@blocknote/core';
 import {BlockNoteView, useBlockNote} from '@blocknote/react';
 import '@blocknote/core/style.css';
+import {useState} from 'react';
 
+// Gets the previously stored editor contents.
+const initialContent: string | null = localStorage.getItem('editorContent');
 // Our <Editor> component we can reuse later
 export default function Editor() {
+  // Stores the editor's contents as an array of Block objects.
+  const [blocks, setBlocks] = useState<Block[] | null>(null);
   // Creates a new editor instance.
-  const editor: BlockNoteEditor | null = useBlockNote({});
+  const editor: BlockNoteEditor | null = useBlockNote({
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+    onEditorContentChange: (editor) => {
+      localStorage.setItem(
+        'editorContent',
+        JSON.stringify(editor.topLevelBlocks)
+      );
+      setBlocks(editor.topLevelBlocks);
+    },
+  });
 
   // Renders the editor instance using a React component.
   return <BlockNoteView editor={editor} />;
